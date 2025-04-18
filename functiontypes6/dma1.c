4791e8066f2a23bc7024c57daf04641ece56ffb4
Yash3946 <patelyash8262@gmail.com>
2025-04-18 11:10:59 +0530

    .

--- functiontypes6/dma1.c
+++ functiontypes6/dma1.c
@@ -8,15 +8,19 @@ int main()
     int *p;
     int *a;
 
-    a=calloc(5,sizeof(int)); //store in array types
-    p=malloc(sizeof(int)); //store 4 bytes
+    p=malloc(sizeof(int)); //store 4 bytes[only 1 block]
+    a=calloc(5,sizeof(int)); //store in array types[parts me devide karega]
  // p=(int*)(malloc(sizeof(int)));
 
     printf("\n enter the number : ");
     scanf("%d",p);
-
     printf("p = %d",*p);
     free(p);
 
+
+    printf("\n enter the number : ");
+    scanf("%d",a);
+    printf("a = %d",*a);
+    free(a);
     return 0;
 }
